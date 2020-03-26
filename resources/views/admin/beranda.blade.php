8e722604dbd33bb400221712c1d8de4b668f7101
KadekIwanAdhiPutra <iwanadhi.id18@gmail.com>
2020-03-26 11:43:33 +0800

    first commit

---
+++ resources/views/admin/beranda.blade.php
@@ -0,0 +1,83 @@
+@extends('layout.admin')
+
+@section('content')
+<!-- Content Header (Page header) -->
+<section class="content-header">
+      <h1>
+        Beranda
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Examples</a></li>
+        <li class="active">Blank page</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+
+      <!-- Default box -->
+
+        <div class="box-body">
+          
+        <div class="box">
+            <div class="box-header">
+
+              <div class="box-tools">
+          
+              </div>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+              <table class="table table-hover">
+                <tbody>
+                <tr>
+                  <th></th>
+                  <th>Biodata</th>
+                  <th></th>
+                  <th></th>
+                  <th></th>
+                  <th>Studi Kasus</th>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>Nama</td>
+                  <td>Kadek Iwan Adhi Putra</td>
+                  <td></td>
+                  <td></td>
+                  <td>Judul</td>
+                  <td>Sistem Pelayanan Apotek Laksamana</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>NIM</td>
+                  <td>1815051021</td>
+                  <td></td>
+                  <td></td>
+                  <td>Pennjelasan</td>
+                  <td>Sistem apotek ini mempermudah karyawan untuk mengecek stok obat dan memudahkan pembeli saat membeli obat</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td>Program Studi</td>
+                  <td>Pendidikan Teknik Informatika</td>
+                  <td></td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td></td>
+                </tr>
+              </tbody></table>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        
+        </div>
+        
+      </div>
+      <!-- /.box -->
+
+    </section>
+    <!-- /.content -->
+@endsection   
